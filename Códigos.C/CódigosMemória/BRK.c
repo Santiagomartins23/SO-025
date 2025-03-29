4b1dd18fd61cf42a23f5a458a88770f513af1cf4
Santiagomartins23 <sanvmarts@gmail.com>
2025-03-29 17:20:42 -0300

    Update BRK.c

--- Códigos.C/CódigosMemória/BRK.c
+++ Códigos.C/CódigosMemória/BRK.c
@@ -3,12 +3,12 @@
 #include <string.h>
 
 int main() {
-    void *heap_inicio = sbrk(0);
+    void *heap_inicio = sbrk(0);  //Obtém o endereço atual da heap
     printf("\nEndereco inicial da heap: %p\n", heap_inicio);
 
     size_t tamanho = 4096;
 
-    void *novo_limite = sbrk(tamanho);
+    void *novo_limite = sbrk(tamanho);  //Expande a heap alocando 4KB
 
     if (novo_limite == (void *)-1) {
         perror("\nFalha ao expandir heap com sbrk");
@@ -17,14 +17,14 @@ int main() {
 
     printf("\nHeap expandida. Novo limite: %p\n", novo_limite);
 
-    memset(novo_limite, 0, tamanho);
+    memset(novo_limite, 0, tamanho);  //Preenche a memória alocada com zero
 
     char *mensagem = (char *)novo_limite;
-    strcpy(mensagem, "Ola, esta string foi armazenada na heap!");
+    strcpy(mensagem, "Ola, esta string foi armazenada na heap!");  //Armazena string na heap
 
     printf("String armazenada na heap: %s\n", mensagem);
 
-    if (sbrk(-tamanho) == (void *)-1) {
+    if (sbrk(-tamanho) == (void *)-1) {  //Reduz a heap ao tamanho original
         perror("Falha ao liberar heap com sbrk");
         return 1;
     }
