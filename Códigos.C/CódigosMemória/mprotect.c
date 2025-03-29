4f162424f4b9a51ae9ff8f424f29ef18ec2ca1d1
Santiagomartins23 <sanvmarts@gmail.com>
2025-03-29 17:18:21 -0300

    Update mprotect.c

--- Códigos.C/CódigosMemória/mprotect.c
+++ Códigos.C/CódigosMemória/mprotect.c
@@ -6,7 +6,7 @@
 
 int main() {
     size_t size = 4096;
-
+    //aloca memoria com mmap
     void *mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (mem == MAP_FAILED) {
         perror("mmap falhou");
@@ -18,7 +18,7 @@ int main() {
     strcpy(mem, "Texto inicial na memoria.");
     printf("\nConteudo inicial na memoria: %s\n", (char *)mem);
 
-    if (mprotect(mem, size, PROT_READ) == -1) {
+    if (mprotect(mem, size, PROT_READ) == -1) { //atualiza memória para somente leitura usando PROT_READ
         perror("\nmprotect falhou ao tornar a memoria somente leitura");
         return 1;
     }
@@ -26,14 +26,14 @@ int main() {
 
     if (mprotect(mem, size, PROT_READ) != -1) {
         printf("\n\nTentando escrever na memoria protegida...\n");
-        strcpy(mem, "Tentativa de escrita em memoria somente leitura.");
+        strcpy(mem, "Tentativa de escrita em memoria somente leitura."); //ao tentar escrever na memória somente com a permissão para leitura, é retornado um segmentation fold
     } else {
         printf("\nA escrita na memoria esta proibida por conta da protecao.\n");
     }
 
     printf("Leitura apos protecao: %s\n", (char *)mem);
 
-    if (mprotect(mem, size, PROT_READ | PROT_WRITE) == -1) {
+    if (mprotect(mem, size, PROT_READ | PROT_WRITE) == -1) { //atualiza memória novamente mas dessa vez para leitura e escrita com PROT_READ e PROT_WRHITE
         perror("\nmprotect falhou ao tornar a memoria gravavel novamente");
         return 1;
     }
@@ -42,7 +42,7 @@ int main() {
     strcpy(mem, "Agora a escrita e permitida novamente.");
     printf("\n\n apos escrever novamente: %s\n", (char *)mem);
 
-    if (mprotect(mem, size, PROT_NONE) == -1) {
+    if (mprotect(mem, size, PROT_NONE) == -1) { //atualiza a memória novamente mas para não ter acesso nem de leitura e nem escrita
         perror("\nmprotect falhou ao tornar a memoria sem permissao de acesso");
         return 1;
     }
@@ -55,7 +55,7 @@ int main() {
         printf("\nNao foi possivel ler a memoria devido a falta de permissao.\n");
     }
 
-    if (mprotect(mem, size, PROT_WRITE) == 0) {
+    if (mprotect(mem, size, PROT_WRITE) == 0) { //atualiza memória para somente escrita
         strcpy(mem, "Tentando escrever em memoria sem permissao.");
     } else {
         printf("\nNao foi possivel escrever na memoria devido a falta de permissao.\n");
